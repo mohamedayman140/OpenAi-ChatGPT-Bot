d6e0562666d5a644b055230f20f2084039b97a89
mohamedayman140 <136701387+mohamedayman140@users.noreply.github.com>
2023-06-20 01:48:14 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -28,7 +28,7 @@ global.BLOCK_CHAT = [] // If you want Block Bot From Chats Use .jid Command and 
 > Ai Bot Password Get From Watching This Video 
 */
 global.AI_PASSWORD = ''
-global.SESSION_ID = ''
+global.SESSION_ID = 'elisa_md@;;;pAEmTLyL#cbBKoVjMQk89QHLVNiXrdR9iFmtesI9ZWR7GWUV_6FI'
 
 /*
 🎲 𝗢𝗧𝗛𝗘𝗥 𝗦𝗘𝗧𝗜𝗡𝗚𝗦
